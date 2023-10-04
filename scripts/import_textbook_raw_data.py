0e06b410ab9332f677c7c45ea68fc262fd2dbdcd
xinruai <xinru.l@rennixue.com.cn>
2023-10-04 16:44:03 +0800

    [UPDATE] Test import textbook data into mongodb

--- scripts/import_textbook_raw_data.py
+++ scripts/import_textbook_raw_data.py
@@ -2,6 +2,9 @@
 Import textbook raw data in data/raw_data_textbook into MongoDB
 """
 import os
+import sys
+sys.path.append("./")
+from lib.db.mongo import MongoClient
 
 
 def load_data():
@@ -11,4 +14,14 @@ def load_data():
         with open(os.path.join(d, file)) as fin:
             text = fin.read()
         contents.append({"file_name": file, "text": text})
-    for i in range(len())
+    for i in range(len(contents)):
+        contents[i]["order_id"] = f"textbook_{i}"
+        contents[i]["sub_code"] = 0
+    return contents
+
+
+mongo_client = MongoClient()
+for content in load_data():
+    status, msg = mongo_client.insert_raw_document(**content)
+    print(status, msg)
+    break
